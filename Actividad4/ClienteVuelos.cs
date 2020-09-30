188634b484920d4d3bed563de61535b8a7fcbae9
171G0247 <171G0247@rcarbonifera.tecnm.mx>
2020-09-30 15:55:26 -0500

    Actividad 4

---
+++ Actividad4/ClienteVuelos.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Actividad4
+{
+    public class ClienteVuelos
+    {
+        HttpClient cliente = new HttpClient();
+        
+        public ClienteVuelos()
+        {
+            cliente.BaseAddress = new Uri("http://vuelos.itesrc.net");
+        }
+        public async void Agregar(DatosVuelos vuelos)
+        {
+
+            if (vuelos.Estado == "A TIEMPO" || vuelos.Estado == "ABORDANDO" || vuelos.Estado == "CANCELADO" || vuelos.Estado == "RETRASADO")
+            {
+                var json = JsonConvert.SerializeObject(vuelos);
+                var result = await cliente.PostAsync("/Tablero", new StringContent(json, Encoding.UTF8, "application/json"));
+                result.EnsureSuccessStatusCode();             
+            }
+            else
+            {
+                MessageBox.Show("El vuelo debe tener un estado");
+            }
+        }
+        public async void Editar(DatosVuelos vuelos)
+        {
+            if (vuelos.Estado == "A TIEMPO" || vuelos.Estado == "ABORDANDO" || vuelos.Estado == "CANCELADO" || vuelos.Estado == "RETRASADO")
+            {
+                var json = JsonConvert.SerializeObject(vuelos);
+                var result = await cliente.PutAsync("/Tablero", new StringContent(json, Encoding.UTF8, "application/json"));
+                result.EnsureSuccessStatusCode();
+            }
+            else
+            {
+                MessageBox.Show("El vuelo debe tener un estado");
+            }
+        }
+        public async void Eliminar(DatosVuelos vuelos)
+        {
+            var json = JsonConvert.SerializeObject(vuelos);
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Delete, "/Tablero");
+            message.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await cliente.SendAsync(message);
+            result.EnsureSuccessStatusCode();
+        }
+
+        public delegate void Invokar();
+        public event Invokar AlInvokar;
+        public IEnumerable<DatosVuelos> model { get; set; }
+        public async void Get()
+        {            
+            var client = new HttpClient();
+            var result = await client.GetAsync("http://vuelos.itesrc.net/Tablero");
+            if (result.IsSuccessStatusCode)
+            {
+                var jsonString = await result.Content.ReadAsStringAsync();
+                model = JsonConvert.DeserializeObject<IEnumerable<DatosVuelos>>(jsonString);
+                AlInvokar?.Invoke();
+            }
+        }
+    }
+}
---
+++ Actividad4/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Actividad4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DatosVuelos datos = new DatosVuelos();
+        ClienteVuelos cliente = new ClienteVuelos();
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = datos;
+            cliente.Get();
+            cliente.AlInvokar += Cliente_AlInvokar;
+            cmbEstado.Items.Add("A TIEMPO");
+            cmbEstado.Items.Add("ABORDANDO");
+            cmbEstado.Items.Add("CANCELADO");
+            cmbEstado.Items.Add("RETRASADO");
+            //
+
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            Editar.IsEnabled = false;
+            Eliminar.IsEnabled = false;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            cliente.Get();
+        }
+
+        private void Cliente_AlInvokar()
+        {
+            dtgDatos.ItemsSource = cliente.model;
+        }
+
+
+        private void Agregar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDestino.Text != "" || txtHora.Text != "" || txtVuelo.Text != "")
+            {
+                try
+                {
+                    datos.Hora = txtHora.Text;
+                    datos.Destino = txtDestino.Text;
+                    datos.Vuelo = txtVuelo.Text;
+                    datos.Estado = cmbEstado.Text;
+                    cliente.Agregar(datos);
+                    timer.Start();
+                    txtDestino.Clear();
+                    txtHora.Clear();
+                    txtVuelo.Clear();
+                    cmbEstado.Text = "";
+                    Editar.IsEnabled = false;
+                    Eliminar.IsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Llene los campos para agregar un nuevo vuelo");
+            }
+        }
+
+        private void Editar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtgDatos.SelectedIndex != -1)
+            {
+                try
+                {
+                    datos.Hora = txtHora.Text;
+                    datos.Destino = txtDestino.Text;
+                    datos.Vuelo = txtVuelo.Text;
+                    datos.Estado = cmbEstado.Text;
+                    cliente.Editar(datos);
+                    cliente.Get();
+                    timer.Start();
+                    txtDestino.Clear();
+                    txtHora.Clear();
+                    txtVuelo.Clear();
+                    cmbEstado.Text = "";
+                    dtgDatos.SelectedItem = null;
+                    Editar.IsEnabled = false;
+                    Eliminar.IsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Es necesario elegir un elemento para poder editarlo.");
+            }
+        }
+
+        private void Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtgDatos.SelectedIndex != -1)
+            {              
+                try
+                {   
+                    DatosVuelos datosEliminar = dtgDatos.SelectedItem as DatosVuelos; 
+                    cliente.Eliminar(datosEliminar);
+                    timer.Start();
+                    txtDestino.Clear(); 
+                    txtHora.Clear();
+                    txtVuelo.Clear(); 
+                    cmbEstado.Text = "";
+                    dtgDatos.SelectedItem = null;
+                    Editar.IsEnabled = false;
+                    Eliminar.IsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Es necesario elegir un elemento para poder eliminarlo." );
+            }
+        }
+        private void dtgDatos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dtgDatos.SelectedItem != null)
+            {
+                Editar.IsEnabled = true;
+                Eliminar.IsEnabled = true;
+                timer.Stop();
+                datos = dtgDatos.SelectedItem as DatosVuelos;
+                txtHora.Text = datos.Hora;
+                txtDestino.Text = datos.Destino;
+                txtVuelo.Text = datos.Vuelo;
+                cmbEstado.Text = datos.Estado;
+            }
+        }
+    }
+}
